ee42a3d646aa0fb138b35e79251fcdc89ce02659
duncangarde <5747810+duncangarde@users.noreply.github.com>
2026-06-11 13:00:53 +0100

    Remove revalidation - needs on demand fix

--- frontend/app/(main)/layout.tsx
+++ frontend/app/(main)/layout.tsx
@@ -5,8 +5,6 @@ import { VisualEditing } from "next-sanity/visual-editing";
 import { draftMode } from "next/headers";
 import { SanityLive } from "@/sanity/lib/live";
 
-export const revalidate = 60;
-
 export default async function MainLayout({
   children,
 }: {

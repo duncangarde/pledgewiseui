9996cca000cd91d5bd251063295af43c5b867f18
duncangarde <5747810+duncangarde@users.noreply.github.com>
2026-06-11 12:37:55 +0100

    Fix internal link issues in navigation

--- frontend/sanity/queries/navigation.ts
+++ frontend/sanity/queries/navigation.ts
@@ -1,9 +1,21 @@
 import { groq } from "next-sanity";
 
+const linkQuery = `
+  _key,
+  ...,
+  "href": select(
+    isExternal => href,
+    defined(href) && !defined(internalLink) => href,
+    @.internalLink->slug.current == "index" => "/",
+    @.internalLink->_type == "post" => "/blog/" + @.internalLink->slug.current,
+    "/" + @.internalLink->slug.current
+  )
+`;
+
 export const NAVIGATION_QUERY = groq`
   *[_type == "navigation"]{
     _type,
     _key,
-    links
+    "links": links[]{${linkQuery}}
   }
 `;
